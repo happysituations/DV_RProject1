557602037f4fd84940c57dc798c7a74537f48ce6
jennawebb117 <jennawebb@utexas.edu>
2015-09-11 15:50:34 -0500

    Added .R file to 01 Data

---
+++ 01 Data/jh.R
@@ -0,0 +1,5 @@
+require("jsonlite")
+require("RCurl")
+# Change the USER and PASS below to be your UTEid
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_jnw653', PASS='orcl_jnw653', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
